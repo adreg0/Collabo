34dd801698a981875afca304c4ad7d37c3f0b910
Snehashish Reddy Manda <ashishreddy9000@gmail.com>
2021-03-14 14:39:46 +0530

    Add extra fields
    Add Field Org_URL
    Add Field Tech
    Add Field Status
    Add Field Projects

--- framework/user/graphql/types/__init__.py
+++ framework/user/graphql/types/__init__.py
@@ -6,23 +6,48 @@ class UserBasicObj(
     description='the user type'
 ):
     id = graphene.ID() 
-    firstName = graphene.String()
-    lastName = graphene.String()
+    email = graphene.email()
+    phone = graphene.String()
+    first_name = graphene.String()
+    last_name = graphene.String()
+    motives = graphene.String()
+    tech = graphene.String()
+    org_url = graphene.String()
+    projects = graphene.String()
+    status = graphene.String()
     city = graphene.String()
-    phoneNo = graphene.String()
+
     
     def resolve_id(self, info):
         if isinstance(self, User):
             return self.id
+    def resolve_email(self, info):
+        if isinstance(self, User):
+            return self.email
+    def resolve_phoneNo(self, info):
+        if isinstance(self, User):
+            return self.phone
     def resolve_firstName(self, info):
         if isinstance(self, User):
             return self.first_name
     def resolve_lastName(self, info):
         if isinstance(self, User):
-            return self.last_name        
+            return self.last_name
+    def resolve_motives(self, info):
+        if isinstance(self, User):
+            return self.motives   
+    def resolve_tech(self, info):
+        if isinstance(self, User):
+            return self.tech_stack  
+    def resolve_org_url(self, info):
+        if isinstance(self, User):
+            return self.org_url   
+    def resolve_projects(self, info):
+        if isinstance(self, User):
+            return self.projects
+    def resolve_status(self, info):
+        if isinstance(self, User):
+            return self.status
     def resolve_city(self, info):
         if isinstance(self, User):
             return self.city
-    def resolve_phoneNo(self, info):
-        if isinstance(self, User):
-            return self.phone
--- framework/user/models.py
+++ framework/user/models.py
@@ -2,19 +2,29 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from framework.utils import GENDER_CHOICES
 
-
 class User(AbstractUser):
     
     id = models.BigAutoField(primary_key=True, null=False)
-    # email address of the user
-    email = models.EmailField(unique=True, null=False, blank=False)
+    # email address of the organization
+    email = models.EmailField(unique=True, null=True, blank=False)
     phone = models.CharField(max_length=15, blank=True, null=True)
-    # varchar(255), stores user's first name
-    first_name = models.CharField(max_length=255, default='', blank=True, verbose_name='First Name')
-    # varchar(255), stores user's last name
+    # varchar(255), Organization's name
+    first_name = models.CharField(max_length=255, default='', blank=False, verbose_name='First Name/Org Name')
     last_name = models.CharField(max_length=255, default='', blank=True, verbose_name='Last Name')
-    # varchar(255), a short self-written bio of the user that will be shown in users' profile
-    bio = models.CharField(max_length=255, default='', blank=True, verbose_name='Bio')
+    # varchar(255), a short self-written bio of the organization that will be shown in the org profile
+    motives = models.CharField(max_length=255, default='', blank=True, verbose_name='Goals and Motives')
+    # The Tech Stacks that the organization uses
+    tech = models.CharField(max_length=255, default='', blank=True, verbose_name='Tech Stack')
+    # The Organization URL
+    org_url = models.URLField(max_length=200, null=True, verbose_name="Organization URL")
+    # The Projects handled by the organization
+    projects = models.CharField(max_length=255, default='', blank=True, verbose_name='Organization Projects')
+    # The Status decided whether the org is looking for contributors or if the user is looking to contribute
+    status = models.CharField(
+        max_length=50,
+        choices=(("1",'Looking for contributors'),("2",'Looking to contribute')),
+        default='1'
+    )
     # Demographic Data
     birthday = models.DateField(null=True, blank=True)
     # gender of the user, should not be shown publicly on profile
@@ -22,4 +32,4 @@ class User(AbstractUser):
     # residence location of the user
     city = models.CharField(max_length=127, default='', blank=True)
     state = models.CharField(max_length=127, default='', blank=True)
-    country = models.CharField(max_length=10, null=False, blank=False, default='IND')
+    country = models.CharField(max_length=10, null=True, blank=False, default='IND')
