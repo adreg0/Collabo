c0f316c04c4ef45501cabf0f099a89d1ade6cd19
adarsh reddy <adarshreddy2003.gali@gmail.com>
2021-03-14 14:48:56 +0530

    frontend

---
+++ frontend/src/views/examples/Icons.js
@@ -0,0 +1,76 @@
+/*!
+
+=========================================================
+* Argon Dashboard React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/argon-dashboard-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/argon-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useState } from "react";
+// react component that copies the given text inside your clipboard
+import { CopyToClipboard } from "react-copy-to-clipboard";
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+// core components
+import Header from "components/Headers/Header.js";
+
+const Icons = () => {
+  const [copiedText, setCopiedText] = useState();
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container className="mt--7" fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="bg-transparent">
+                <h3 className="mb-0">Repositories</h3>
+              </CardHeader>
+              <CardBody>
+                <Row className="icon-examples">
+                  <Col lg="3" md="6">
+                      <button
+                        className="btn-icon-clipboard"
+                        type="button"
+                        onClick={()=>{
+                          window.open("https://github.com/amFOSS","_blank").focus()
+                        }}
+                      >
+                        <div>
+                          <i className="ni ni-active-40" />
+                          <span>amFOSS</span>
+                        </div>
+                      </button>
+                    
+                  </Col>
+                  
+                </Row>
+              </CardBody>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default Icons;
